6127b6efd332c853a4eb75f095930d11cfa1364b
kevinjoythomas <kevinjoythomas2004@gmail.com>
2024-10-10 23:19:29 +0530

    loaded data

--- src/get_data.py
+++ src/get_data.py
@@ -14,13 +14,13 @@ def read_params(config_path):
         
 def get_data(config_path):
     config = read_params(config_path)
-    
     data_path = config["base"]["data_source"]["s3_source"]
     df = pd.read_csv(data_path,sep = ",",encoding='utf-8')
-    print(df.head())
+    return df
+
 if __name__ == "__main__":
-    
     args = argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
     parsed_args = args.parse_args()
-    get_data(config_path=parsed_args.config)
+    data = get_data(config_path=parsed_args.config)
+    
