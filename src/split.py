358d8bd723e9e3484760df66fb180773e0df5f32
kevinjoythomas <kevinjoythomas2004@gmail.com>
2024-10-10 23:34:22 +0530

    split stage added

--- src/split.py
+++ src/split.py
@@ -14,3 +14,21 @@ def split_save(config_path):
     raw_data_path = config['load_data']['raw_dataset_csv']
     split_ratio = config['split_data']['test_size']
     random_state =  config['base']['random_state']
+    
+    df = pd.read_csv(raw_data_path,sep = ",")
+    train,test = train_test_split(
+        df,
+        test_size=split_ratio,
+        random_state=42
+        )
+    
+    train.to_csv(train_data_path,sep = ",",index = False)
+    test.to_csv(test_data_path,sep = ",",index = False)
+    
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args = args.parse_args()
+    split_save(config_path=parsed_args.config)
+    
+    
