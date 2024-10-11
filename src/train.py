9d7c7817f040cdb5caf8b1df13392da0bf13887a
kevinjoythomas <kevinjoythomas2004@gmail.com>
2024-10-12 04:41:42 +0530

    mlflow integrated

--- src/train.py
+++ src/train.py
@@ -13,6 +13,8 @@ from get_data import read_params
 import argparse
 import joblib
 import json 
+import mlflow
+from urllib.parse import urlparse
 
 def eval(pred, actual):
     rmse = np.sqrt(mean_squared_error(actual, pred))
@@ -40,38 +42,57 @@ def train(config_path):
     train_x = train.drop(target_col,axis = 1)
     test_x = test.drop(target_col,axis = 1)
     
-    lr = ElasticNet(
-        alpha=alpha,
-        l1_ratio=l1_ratio,
-        random_state=random_state
-    )
-    
-    lr.fit(train_x,train_y)
-    
-    elastic_predicted = lr.predict(test_x)
-    (rmse, mae, r2) = eval(elastic_predicted,test_y)
+    ######### mlflow
+    mlflow_config = config["mlflow_config"]
+    remote_server_uri = mlflow_config['remote_server_url']
+    print(remote_server_uri)
 
-    os.makedirs(model_dir, exist_ok=True)
-    model_path = os.path.join(model_dir,"model.joblib")
-    joblib.dump(lr,model_path)
-    
-    scores_file = config['reports']['scores']
-    params_file = config['reports']['params']
-    with open(scores_file,"w") as f:
-        scores = {
-            "rmse":rmse,
-            "mae":mae,
-            "r2":r2
-        }
-        json.dump(scores,f, indent=4)
+    # Set the MLflow tracking URI to the remote server
+    mlflow.set_tracking_uri(remote_server_uri)
+    print("setting experiment")
 
-    with open(params_file,"w") as f:
-        params = {
-            "L1_ratio":l1_ratio,
-            "alpha":alpha,
-            "random_state":random_state
-        }
-        json.dump(params,f, indent=4)
+    # Start the MLflow run
+    with mlflow.start_run(run_name=mlflow_config['run_name']) as mlops_run:
+        
+        # Train the model
+        lr = ElasticNet(
+            alpha=alpha,
+            l1_ratio=l1_ratio,
+            random_state=random_state
+        )
+        
+        lr.fit(train_x, train_y)
+        
+        # Make predictions and evaluate
+        elastic_predicted = lr.predict(test_x)
+        (rmse, mae, r2) = eval(elastic_predicted, test_y)
+
+        # Log parameters and metrics to MLflow
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+        
+        # Get the tracking URI scheme (file or http)
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+        print("tracking url")
+
+        # Log the model based on the artifact storage type
+        if tracking_url_type_store != "file":
+            # Log the model to the remote tracking server
+            mlflow.sklearn.log_model(
+                lr,
+                "model",
+                registered_model_name=mlflow_config["registered_model_name"]
+            )
+        else:
+            # Log the model locally (in case of file storage)
+            mlflow.sklearn.log_model(lr, "model")
+
+
+   
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
